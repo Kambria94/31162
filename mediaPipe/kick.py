c9636a351b5a1b70962efb5ceb529914b199d8f9
nikhil345678 <152929564+nikhil345678@users.noreply.github.com>
2023-12-15 17:15:00 -0500

    Update kick.py

--- mediaPipe/kick.py
+++ mediaPipe/kick.py
@@ -28,10 +28,15 @@ def calculate_angle(a,b,c):
 
 snaptime = 0
 start_snap = 0
+numlarmgoodkneeup = 0
+numrbackgoodkneeup = 0
+numrleggoodkneeup = 0
+numlleggoodkneeup = 0
+numkneeup = 0
 
 #Video feed
 img = cv2.VideoCapture(0)
-
+rkneeup = False
 
 #setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -54,9 +59,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         try:
             landmarks = results.pose_landmarks.landmark
             rshoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            rshoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
             rhip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP].x, landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
             rknee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE].x, landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
             rankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE].x, landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+            rheel = [landmarks[mp_pose.PoseLandmark.RIGHT_HEEL].x, landmarks[mp_pose.PoseLandmark.RIGHT_HEEL.value].y]
+            lheel = [landmarks[mp_pose.PoseLandmark.LEFT_HEEL].x, landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].y]
             lwrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
             lelbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
             lshoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
@@ -64,20 +72,41 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             lknee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
             lankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
             #Measurements
-            rknee = calculate_angle(rhip, rknee , rankle)
-            rhip = calculate_angle(rshoulder, rhip, rknee)
-            kneeup = False
-            snap = False
-            
-            if (rknee <= 130 and rhip <= 130) and kneeup == False:
+            ang_larm = calculate_angle(lwrist, lelbow, lshoulder)
+            ang_rback = calculate_angle(rshoulder, rhip, rknee)
+            ang_rleg = calculate_angle(rhip, rknee , rankle)
+            ang_lleg = calculate_angle(lhip, lknee, lankle,)
+            heel_dist = np.sqrt((rheel[0] - lheel[0])**2 + (rheel[1] - lheel[1])**2)
+            #true/false statements
+            larmgood = False
+            rbackgood = False
+            rleggood = False
+            lleggood = False
+            heelgood = False
+            if ang_rleg <= 130 and ang_rback <= 130 and rkneeup == False:
                 print ("Knee Up")
                 start_snap = datetime.datetime.timestamp(datetime.datetime.now())
-                kneeup = True
-            if kneeup == True:
-                if rknee >= 160:
+                rkneeup = True
+                
+            if rkneeup == True:
+                numkneeup += 1
+                if ang_larm > 60 and ang_larm < 130:
+                    numlarmgoodkneeup += 1
+                if ang_rback < 110 and ang_rback > 75:
+                    numrbackgoodkneeup += 1
+                if ang_rleg < 120 and ang_rleg > 75:
+                    numrleg
+                if ang_lleg < 180 and ang_lleg > 145:
+                    lleggood = True
+                if ang_rleg >= 160:
+                    print("Snap")
+                    snaptime = datetime.datetime.timestamp(datetime.datetime.now())-start_snap
                     snap = True
-                    kneeup = False
-            #if snap == True and 
+                    rkneeup = False
+            if snap == True and rhip >= 160 and rknee >= 160:
+                print("leg down")
+                legdown = True
+            
 
            
             #distance = np.sqrt((rshoulder[0] - rwrist[0])**2 + (rshoulder[1] - rwrist[1])**2)

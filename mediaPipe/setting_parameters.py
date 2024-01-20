1eba70cbcdcf58f004a99572c3fcaf96d763e5fd
udaysrk <uday@terratrue.com>
2024-01-20 17:03:58 -0500

    Added project logo and colors

--- mediaPipe/setting_parameters.py
+++ mediaPipe/setting_parameters.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import PIL.ImageTk as ImageTk
+import PIL.Image as Image
 
 def save_values():
     # Function to save the entered values
@@ -16,29 +18,38 @@ def save_values():
 
 # Create the main window
 window = tk.Tk()
+#fg_color_str = "yellow2"
+fg_color_str = "Gold2"
+bg_color_str = "MediumOrchid4"
+window.configure(bg=bg_color_str)
 window.title("Pose Perfect - Set Parameters")
 
 # Labels and initial values
-labels = ["Elbow Angle", "Hip/Back Angle", "Left Knee Angle", "Right Knee Angle", "Heel Distance (Leg to length ratio)"]
+labels = ["Elbow Angle (deg)", "Hip/Back Angle (deg)", "Left Knee Angle (deg)", "Right Knee Angle (deg)", "Heel Distance to Leg length Ratio"]
 min_values = [165, 0, 148, 161, 0.455]
 max_values = [180, 180, 162, 172, 0.909]
 
 label_font_str = 'Helvetica 12 bold'
 # Header row
-tk.Label(window, text="Configure", font=label_font_str).grid(row=0, column=0, pady=10)
-tk.Label(window, text="Minimum", font=label_font_str).grid(row=0, column=1, pady=10)
-tk.Label(window, text="Maximum", font=label_font_str).grid(row=0, column=2, pady=10)
+image = Image.open("pose_perfect.png")
+image = image.resize((116, 30), Image.ANTIALIAS)
+photo = ImageTk.PhotoImage(image)
+tk.Label(window, image=photo).grid(row=0, column=1, padx=10, pady=10)
 
+tk.Label(window, text="Parameter", font=label_font_str, fg=fg_color_str, bg=bg_color_str).grid(row=1, column=0, pady=10)
+tk.Label(window, text="Minimum", font=label_font_str, fg=fg_color_str, bg=bg_color_str).grid(row=1, column=1, pady=10)
+tk.Label(window, text="Maximum", font=label_font_str, fg=fg_color_str, bg=bg_color_str).grid(row=1, column=2, pady=10)
+header_count = 2
 # Create and place labels and entry widgets
 entries = []
 for label, min_value, max_value in zip(labels, min_values, max_values):
-    tk.Label(window, text=label, font=label_font_str).grid(row=len(entries)+1, column=0, sticky="e")
+    tk.Label(window, text=label, font=label_font_str, fg=fg_color_str, bg=bg_color_str).grid(row=len(entries)+header_count, column=0, sticky="e")
     entry = tk.Entry(window)
     entry.insert(0, str(min_value))
-    entry.grid(row=len(entries)+1, column=1, sticky="w")
+    entry.grid(row=len(entries)+header_count, column=1, sticky="w")
     entry2 = tk.Entry(window)
     entry2.insert(0, str(max_value))
-    entry2.grid(row=len(entries)+1, column=2, sticky="w")
+    entry2.grid(row=len(entries)+header_count, column=2, sticky="w")
     entries.append((entry, entry2))
 
 # Button to save values
